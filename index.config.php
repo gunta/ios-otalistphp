d168097c228063ea6d69cf4a34e5cd3f553bc18c
Gunther Brunner <gunta85@gmail>
2012-09-13 12:18:22 +0900

    * Including first files.

---
+++ index.config.php
@@ -0,0 +1,16 @@
+<?php
+    $config = array(
+        // Title for the page
+        "title" => "iOS OTA Downloads",
+
+        // Parses each plist file
+        "parsePlist" => true,
+
+        // Base local path for scanning for plist/ipa files
+        "localPath" => ".",
+
+        // Base url for the plist/ipa files with trailing slash. Not needed if parsePlist == true.
+        "baseUrl" => "http://.../"
+    );
+
+?>
